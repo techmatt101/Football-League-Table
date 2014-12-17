41610c464bd63c250f4a5a556cbc85a88d908c21
techmatt101 <matt@kempmail.co.uk>
2014-12-17 06:52:30 +0000

    Select team now saves user choice and ui tweaks

---
+++ MVC/Models/LeagueTable/SelectTeamView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FootballLeagueTable.Models.LeagueTable
+{
+    public class SelectTeamView
+    {
+        [Display(Name = "Following Team")]
+        public int SelectedFollowingTeam { get; set; }
+        public IEnumerable<SelectListItem> FollowingTeamList { get; set; }
+
+        [Display(Name = "Following Team")]
+        public int SelectedRivalTeamList { get; set; }
+        public IEnumerable<SelectListItem> RivalTeamList { get; set; }
+    }
+}
